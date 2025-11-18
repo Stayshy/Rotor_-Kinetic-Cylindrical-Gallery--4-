f7294ac5dcf82c0b64598d1f9f7ba18ade2ac49e
Stayshy <n-kharitosha@yandex.ru>
2025-11-18 17:10:50 +0300

    WORKWORKWORK

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import React from 'react';  // <-- Add this
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
